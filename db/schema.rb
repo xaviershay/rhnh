de069da0372bbce9472331d9fba38e66130cf5ca
Xavier Shay <xavier@rhnh.net>
2008-04-12 17:19:57 +1000

    Renumber openid migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 7) do
+ActiveRecord::Schema.define(:version => 8) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id",                                    :null => false
