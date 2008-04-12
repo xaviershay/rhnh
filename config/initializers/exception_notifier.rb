ba38af37707a18403e67bb90504420a8fc45d3c9
Xavier Shay <xavier@rhnh.net>
2008-04-12 13:45:57 +1000

    Exception mailer config

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,1 +1,7 @@
 ExceptionNotifier.exception_recipients = [Enki::Config.new("config/enki.yml")[:author, :email]]
+ExceptionNotifier.smtp_settings.merge!(
+  :address   => 'mail.messagingengine.com',
+  :user_name => Base64.decode64("eGF2aWVyQGZhc3RtYWlsLmZt\n"),
+  :password  => Base64.decode64("aDR5d2hhdHNnb2luZ29u\n"),
+  :authentication => :plain
+)
