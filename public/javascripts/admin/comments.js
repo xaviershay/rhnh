a5fb39648445b6f903a4528f637943c2cb0df3d6
Xavier Shay <xavier@rhnh.net>
2008-05-17 12:03:12 +1000

    Fixup comment spam JS

--- public/javascripts/admin/comments.js
+++ public/javascripts/admin/comments.js
@@ -1,5 +1,7 @@
 function spamBehaviour(enableAlternateFunc) {
   return function () {
+    $(this).find('input').attr('src', '/images/silk/flag_grey.png').attr('disabled', true);
+    enableAlternateFunc($(this).parent('td'));
     $.ajax({
       type: "POST",
       url: $(this).attr('action'),
@@ -13,8 +15,6 @@ function spamBehaviour(enableAlternateFunc) {
         if (msg.undo_path)
           display += '<span class="undo-link"> (<a class="undo-link" href="' + msg.undo_path + '">undo</a>)</span>';
         humanMsg.displayMsg(display);
-        $(this).find('.spam-item input').attr('src', '/images/silk/flag_grey.png').attr('disabled', true);
-        enableAlternateFunc();
       },
       error: function (XMLHttpRequest, textStatus, errorThrown) {
         humanMsg.displayMsg( 'Could not alter comment' );
@@ -26,6 +26,6 @@ function spamBehaviour(enableAlternateFunc) {
 $(document).ready(function() {
   destroyAndUndoBehaviour('comments')();
 
-  $('form.ham').submit(spamBehaviour(function() { $('form.spam input').attr('src', '/images/silk/flag_orange.png').attr('disabled', false) }));
-  $('form.spam').submit(spamBehaviour(function() { $('form.ham input').attr('src', '/images/silk/flag_green.png').attr('disabled', false) }));
+  $('form.ham').submit( spamBehaviour(function(e) { e.find('form.spam input').attr('src', '/images/silk/flag_orange.png').attr('disabled', false) }));
+  $('form.spam').submit(spamBehaviour(function(e) { e.find('form.ham  input').attr('src', '/images/silk/flag_green.png' ).attr('disabled', false) }));
 }); // Defined in admin/common.js
