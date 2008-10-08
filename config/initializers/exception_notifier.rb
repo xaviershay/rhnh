a5d02f97d6831e7305545588c3ec54a024ef8d18
Xavier Shay <xavier@rhnh.net>
2008-10-07 21:06:50 -0700

    ooooh major fail, let's not put my password out there for all to see

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,7 +1,2 @@
 ExceptionNotifier.exception_recipients = [Enki::Config.new("config/enki.yml")[:author, :email]]
-ExceptionNotifier.smtp_settings.merge!(
-  :address   => 'mail.messagingengine.com',
-  :user_name => Base64.decode64("eGF2aWVyQGZhc3RtYWlsLmZt\n"),
-  :password  => Base64.decode64("aDR5d2hhdHNnb2luZ29u\n"),
-  :authentication => :plain
-)
+require 'config/mailer'
