bfb035c95f50be5567122e048594bccd2a92621f
Xavier Shay <xavier@rhnh.net>
2008-12-27 22:28:47 -0800

    Sort and downcase tags

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -1,5 +1,5 @@
 module TagHelper
   def linked_tag_list(tags)
-    tags.collect {|tag| link_to(h(tag.name), posts_path(:tag => tag))}.join(", ")
+    tags.sort_by {|x| x.name.downcase }.collect {|tag| link_to(h(tag.name.downcase), posts_path(:tag => tag))}.join(", ")
   end
 end
