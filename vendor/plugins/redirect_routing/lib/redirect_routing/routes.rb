3f6ac5374ef550d87ded2e63771306f02c61b1ce
Xavier Shay <xavier@rhnh.net>
2011-01-16 17:18:53 +1100

    Remove unused redirect_routing plugin

--- vendor/plugins/redirect_routing/lib/redirect_routing/routes.rb
+++
@@ -1,7 +0,0 @@
-module RedirectRouting
-  module Routes
-    def redirect(path, *args)
-      connect path, :controller => "redirect_routing", :action => "redirect", :args => args
-    end
-  end
-end
