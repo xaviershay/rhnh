82b32a93064d256acc96ccaee20ca79f391fa032
Xavier Shay <xavier@rhnh.net>
2011-01-24 12:48:17 +1100

    Don't raise an error email on InvalidAuthenticityToken

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,5 +1,6 @@
 Enki::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[Enki] ",
-  :sender_address => [Enki::Config.default[:author, :email]],
+  :ignore_exceptions    => [ActionController::InvalidAuthenticityToken],
+  :email_prefix         => "[Enki] ",
+  :sender_address       => [Enki::Config.default[:author, :email]],
   :exception_recipients => [Enki::Config.default[:author, :email]]
 require "#{RAILS_ROOT}/config/mailer"
