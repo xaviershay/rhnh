7bc7dea6c137934ac1a943309c75ebf03040394e
Xavier Shay <xavier@rhnh.net>
2011-01-28 07:25:47 +1100

    Ignore some more exceptions that I don't care about

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,5 +1,5 @@
 Enki::Application.config.middleware.use ExceptionNotifier,
-  :ignore_exceptions    => [ActionController::InvalidAuthenticityToken],
+  :ignore_exceptions    => [ActionController::InvalidAuthenticityToken, ActiveRecord::RecordNotFound, ActionView::MissingTemplate],
   :email_prefix         => "[Enki] ",
   :sender_address       => [Enki::Config.default[:author, :email]],
   :exception_recipients => [Enki::Config.default[:author, :email]]
