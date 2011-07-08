9b4dd4807177584bfde2bac8d812e56aabd200ce
Xavier Shay <xavier@rhnh.net>
2011-07-09 09:35:24 +1000

    Bring back exception notification with sendgrid.

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'coderay'
 gem 'lesstile'
 gem 'formtastic'
 gem "will_paginate", "~> 3.0.pre2"
+gem "exception_notification_rails3", '~> 1.1', :require => 'exception_notifier'
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
---
+++ config/initializers/exception_notifier.rb
@@ -0,0 +1,5 @@
+Enki::Application.config.middleware.use ExceptionNotifier,
+  :ignore_exceptions    => [ActionController::InvalidAuthenticityToken, ActiveRecord::RecordNotFound, ActionView::MissingTemplate],
+  :email_prefix         => "[Enki] ",
+  :sender_address       => [Enki::Config.default[:author, :email]],
+  :exception_recipients => [Enki::Config.default[:author, :email]]
