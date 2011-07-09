3e525cbb4f7494ad12c63d5e4c1dab2237ae5047
Xavier Shay <xavier@rhnh.net>
2011-07-09 10:42:32 +1000

    Correct production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Enki::Application.configure do
     :domain         => ENV['SENDGRID_DOMAIN']
   }
 
-  use Rack::Cache,
+  config.middleware.use Rack::Cache,
     :verbose => true,
     :metastore => "memcached://#{ENV['MEMCACHE_SERVERS']}",
     :entitystore => "memcached://#{ENV['MEMCACHE_SERVERS']}"
