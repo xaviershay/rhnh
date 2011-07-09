95da16cad253b75c30688ccdbb6fcff732418282
Xavier Shay <xavier@rhnh.net>
2011-07-09 12:42:45 +1000

    Caching of pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def show
     @page = Page.find_by_slug(params[:id]) || raise(ActiveRecord::RecordNotFound)
+    fresh_when last_modified: @page.updated_at.utc, public: true
   end
 end
